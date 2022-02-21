990ba46cd79d5b6d60992df735328dbbdc17e283
Lynda Foster <lfost42@wgu.edu>
2022-02-21 12:44:07 -0800

    added user identity

--- SoftwarePlannerUI/Data/ApplicationDbContext.cs
+++ SoftwarePlannerUI/Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using DataAccessLibrary.Models;
 
 namespace SoftwarePlannerUI.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Users>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
