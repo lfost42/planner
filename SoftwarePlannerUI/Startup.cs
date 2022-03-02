c2379cc5611aef794fc398eacaa00d20f29eda51
Lynda Foster <lfost42@wgu.edu>
2022-03-01 23:29:28 -0800

    update nuget package

--- SoftwarePlannerUI/Startup.cs
+++ SoftwarePlannerUI/Startup.cs
@@ -28,7 +28,7 @@ namespace SoftwarePlannerUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
