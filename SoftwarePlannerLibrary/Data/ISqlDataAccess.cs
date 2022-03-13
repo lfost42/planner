0d9d97aef4189663a9765655932ac1ad633d8eb2
Lynda Foster <lfost42@wgu.edu>
2022-03-13 15:37:57 -0700

    sql data access

---
+++ SoftwarePlannerLibrary/Data/ISqlDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SoftwarePlannerLibrary.Data
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement,
+                               U parameters,
+                               string connectionStringName,
+                               bool isStoredProcedure = false);
+        void SaveData<T>(string sqlStatement,
+                         T parameters,
+                         string ConnectionStringName,
+                         bool isStoredProcedure = false);
+    }
+}
