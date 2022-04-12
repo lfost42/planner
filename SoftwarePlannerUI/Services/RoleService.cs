9aab0b3c564a2100ef8c69086d65d7c6d0d9e08f
Lynda Foster <lfost42@wgu.edu>
2022-04-11 21:27:00 -0700

    added role service

---
+++ SoftwarePlannerUI/Services/RoleService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SoftwarePlannerUI.Data;
+using SoftwarePlannerUI.Models;
+using SoftwarePlannerUI.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwarePlannerUI.Services
+{
+    public class RoleService : IRolesService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<UserModel> _userManager;
+        public RoleService(ApplicationDbContext context,
+                            RoleManager<IdentityRole> roleManager,
+                            UserManager<UserModel> userManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        
+        public async Task<bool> AddUserToRoleAsync(UserModel user, string roleName)
+        {
+            return (await _userManager.AddToRoleAsync(user, roleName)).Succeeded;
+
+        }
+
+        public async Task<string> GetRoleNameById(string roleId)
+        {
+            return await _roleManager.GetRoleNameAsync(_context.Roles.Find(roleId));
+        }
+
+        public async Task<IEnumerable<string>> GetUserRolesAsync(UserModel user)
+        {
+            return await _userManager.GetRolesAsync(user);
+
+        }
+
+        public async Task<bool> IsUserInRoleAsync(UserModel user, string roleName)
+        {
+            return await _userManager.IsInRoleAsync(user, roleName);
+        }
+
+        public async Task<bool> RemoveUserFromRoleAsync(UserModel user, string roleName)
+        {
+            return (await _userManager.RemoveFromRoleAsync(user, roleName)).Succeeded;
+        }
+
+        public async Task<bool> RemoveUserFromRolesAsync(UserModel user, IEnumerable<string> roles)
+        {
+            return (await _userManager.RemoveFromRolesAsync(user, roles)).Succeeded;
+        }
+    }
+}
