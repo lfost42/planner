f577677a9de81fd3a8d53267def1f86f35e6d044
Lynda Foster <lfost42@wgu.edu>
2022-05-31 19:17:25 -0700

    rebuild server

--- Planner/server/server.js
+++ Planner/server/server.js
@@ -1,19 +1,51 @@
-const tasks = require("./routes/tasks");
-const mongoose = require('mongoose')
-const cors = require("cors");
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
+
 app.use(express.json());
-
-const db='mongodb://localhost:27017/tasks'
-
-mongoose.connect(db, ({ useNewUrlParser: true}))
-	.then(console.log('Connected to MongoDb'))
-	.catch(err => console.log(err))
-
 app.use(cors());
 
-app.use("/api/tasks", tasks);
+mongoose.connect('mongodb://localhost:27017/task', {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+}).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const Task = require('./models/Task')
+
+app.get('/tasks', async (req, res) => {
+	const tasks = await Task.find()
+	res.json(tasks)
+})
+
+app.post('/task/new', (req, res) => {
+	const task = new Task({
+		text: req.body.text
+	})
+	task.save()
+	res.json(task)
+})
+
+app.delete('/task/delete/:id', async (req, res) => {
+	const result = await Task.findByIdAndDelete(req.params.id)
+	res.json({result})
+})
+
+app.get('/task/complete/:id', async (req, res) => {
+	const task = await Task.findById(req.params.id)
+	task.complete = !task.complete
+	task.save()
+	res.json(task)
+})
+
+app.put('/task/update/:id', async (req, res) => {
+	const task = await Task.findById(req.params.id)
+	if (task.text != null) {
+		task.text = req.body.text
+	}
+	task.save()
+	res.json(task)
+});
+
+app.listen(3001)
